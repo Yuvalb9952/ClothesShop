a337b2cb4ef245f0122b6a57d6c45b38f9af12c9
Eden <314713934.idf.il>
2021-07-16 16:35:12 +0300

    add admin sign in

---
+++ ClothesShop/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ClothesShop.Models;
+using ClothesShop.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.FileProviders;
+
+namespace final_project.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Welcome()
+        {
+            if (HttpContext.Session.GetInt32("adminId") == null)
+            {
+                return View("Views/Users/NotFound.cshtml");
+            }
+            ViewBag.Name = HttpContext.Session.GetString("userName");
+            return View();
+        }
+    }
+}
