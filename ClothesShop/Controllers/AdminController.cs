959e5b8a3d1216b5477cb0c574ec677a5b242b9f
Eden <314713934.idf.il>
2021-07-16 16:50:12 +0300

    add customers view in admin session

--- ClothesShop/Controllers/AdminController.cs
+++ ClothesShop/Controllers/AdminController.cs
@@ -16,6 +16,21 @@ namespace final_project.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly ClothesShopContext _context;
+
+        public AdminController(ClothesShopContext context)
+        {
+            _context = context;
+        }
+
+        public class CostumerViewModel
+        {
+            public string FullName { get; set; }
+            public string Gender { get; set; }
+            public string Email { get; set; }
+            public int OrdersNumber { get; set; }
+        }
+
         public IActionResult Welcome()
         {
             if (HttpContext.Session.GetInt32("adminId") == null)
@@ -23,6 +38,32 @@ namespace final_project.Controllers
                 return View("Views/Users/NotFound.cshtml");
             }
             ViewBag.Name = HttpContext.Session.GetString("userName");
+            return View();
+        }
+
+        public IActionResult Costumers()
+        {
+            if (HttpContext.Session.GetInt32("adminId") == null)
+            {
+                return View("Views/Users/NotFound.cshtml");
+            }
+
+            List<Customer> costumers = _context.Costumers.ToList();
+            List<Order> orders = _context.Orders.ToList();
+            List<CostumerViewModel> costumerView = orders.GroupBy(g => g.Customer.Email)
+                .Select(g =>
+                {
+                    var costumer = costumers.Single(c => c.Email == g.Key);
+                    return new CostumerViewModel
+                    {
+                        FullName = $"{costumer.FirstName} {costumer.LastName}",
+                        Gender = costumer.Gender,
+                        Email = costumer.Email,
+                        OrdersNumber = g.Count()
+                    };
+                }).ToList();
+
+            ViewBag.CostumersView = costumerView;
             return View();
         }
     }
