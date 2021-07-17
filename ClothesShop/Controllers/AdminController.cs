32945cb6f86b820a22d60a2bd6edf2a344f1059a
Eden <314713934.idf.il>
2021-07-17 15:17:11 +0300

    edit add delete products in admin session

--- ClothesShop/Controllers/AdminController.cs
+++ ClothesShop/Controllers/AdminController.cs
@@ -17,10 +17,12 @@ namespace final_project.Controllers
     public class AdminController : Controller
     {
         private readonly ClothesShopContext _context;
+        private readonly IWebHostEnvironment webHostEnvironment;
 
-        public AdminController(ClothesShopContext context)
+        public AdminController(ClothesShopContext context, IWebHostEnvironment iwebHostEnvironment) 
         {
             _context = context;
+            webHostEnvironment = iwebHostEnvironment;
         }
 
         public class CostumerViewModel
@@ -41,6 +43,99 @@ namespace final_project.Controllers
             return View();
         }
 
+        #region Products
+
+        public IActionResult EditProducts()
+        {
+            if (HttpContext.Session.GetInt32("adminId") == null)
+            {
+                return View("Views/Users/NotFound.cshtml");
+            }
+
+            List<Product> products = _context.Products.ToList();
+            List<Category> categories = _context.Categories.Where(cat => !cat.IsDeleted).ToList();
+
+            ViewBag.Products = products;
+            ViewBag.Categories = categories;
+            return View();
+        }
+
+        public IActionResult RemoveProduct(int id)
+        {
+            if (HttpContext.Session.GetInt32("adminId") == null)
+            {
+                return View("Views/Users/NotFound.cshtml");
+            }
+
+            _context.Products.Single(p => p.Id == id).IsDeleted = true;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                TempData["ProductRemovalFailed"] = true;
+            }
+
+            return Redirect("/Admin/EditProducts");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddProduct(string name, int category, int price, IFormFile img)
+        {
+            if (HttpContext.Session.GetInt32("adminId") == null)
+            {
+                return View("Views/Users/NotFound.cshtml");
+            }
+
+            string pathToSave = await SaveImageFile(img, name);
+
+            Product newProduct = new Product()
+            {
+                Name = name,
+                Category = _context.Categories.Single(c => c.Id == category),
+                Price = price,
+                ImageSrc = pathToSave
+            };
+
+            _context.Add(newProduct);
+            await _context.SaveChangesAsync();
+            return Redirect("/Admin/EditProducts");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditProduct(int id,
+                                                     string name,
+                                                     int category,
+                                                     int price,
+                                                     IFormFile img)
+        {
+            if (HttpContext.Session.GetInt32("adminId") == null)
+            {
+                return View("Views/Users/NotFound.cshtml");
+            }
+
+            Product productToEdit = _context.Products.Single(p => p.Id == id);
+            productToEdit.Name = name;
+            productToEdit.Category = _context.Categories.Single(c => c.Id == category);
+            productToEdit.Price = price;
+
+            if (img != null)
+            {
+                productToEdit.ImageSrc = await SaveImageFile(img, name);
+            }
+
+            _context.Update(productToEdit);
+            await _context.SaveChangesAsync();
+            return Redirect("/Admin/EditProducts");
+        }
+
+        #endregion
+
+        #region Costumers
         public IActionResult Costumers()
         {
             if (HttpContext.Session.GetInt32("adminId") == null)
@@ -67,6 +162,9 @@ namespace final_project.Controllers
             return View();
         }
 
+        #endregion
+
+        #region Admins
         public IActionResult Admins()
         {
             if (HttpContext.Session.GetInt32("adminId") == null)
@@ -123,6 +221,32 @@ namespace final_project.Controllers
             _context.Add(AdminToAdd);
             _context.SaveChanges();
             return Redirect("/Admin/Admins");
+        }
+
+        #endregion
+
+        private async Task<string> SaveImageFile(IFormFile img, string name)
+        {
+            // Create a File Info 
+            FileInfo fi = new FileInfo(img.FileName);
+
+            // This code creates a unique file name to prevent duplications 
+            // stored at the file location
+            var newFilename = name + "_" + String.Format("{0:d}",
+                              (DateTime.Now.Ticks / 10) % 100000000) + fi.Extension;
+            var webPath = webHostEnvironment.WebRootPath;
+            var path = Path.Combine("", webPath + @"\ImageFiles\" + newFilename);
+
+            // IMPORTANT: The pathToSave variable will be save on the column in the database
+            var pathToSave = @"/ImageFiles/" + newFilename;
+
+            // This stream the physical file to the allocate wwwroot/ImageFiles folder
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await img.CopyToAsync(stream);
+            }
+
+            return pathToSave;
         }
 
     }
