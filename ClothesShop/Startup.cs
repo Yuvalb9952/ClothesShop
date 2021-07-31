84010a3eb78a66f230083e9a587a19ad642b3400
Adi Papi <adipapismedov@gmail.com>
2021-07-31 15:15:35 +0300

    Add Razor Runtime Compilation

--- ClothesShop/Startup.cs
+++ ClothesShop/Startup.cs
@@ -24,6 +24,7 @@ namespace ClothesShop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
